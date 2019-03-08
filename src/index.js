16f22b8132b99e206a1defd41a7ec80ee55cf947
Thomas Drevon <thomas@sanity.io>
2019-03-08 10:30:27 +0100

    Sane default props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import sanityClient from 'part:@sanity/base/client'
 import styles from './Orbitals2d.css'
 import {initializeWorld, addBody} from './generator'
 const elementId = 'orbitals2dWorld'
 
-function fetch() {
+function fetch(query) {
   return sanityClient.fetch(query)
 }
 
 class Orbitals2d extends React.Component {
+  static propTypes = {
+    query: PropTypes.string,
+    transformDocument: PropTypes.func.isRequired,
+    attractorColor: PropTypes.string
+  }
+  static defaultProps = {
+    query: '*[]|order(_createdAt desc)[0..10]',
+    attractorColor: '#606060'
+  }
 
   componentDidMount = () => {
     const {query, transformDocument, attractorColor} = this.props
